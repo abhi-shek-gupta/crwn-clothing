9b3b40a96a8425b90cca099bfccc96c4c48a8ec8
Abhishek Gupta <abhishek.gupta11@allocatesoftware.com>
2020-01-05 19:21:14 +0530

    upadate our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => {
+  return (<div>Welome to Hats Page</div>)
+}
 function App() {
-  return ( <div ><Homepage/> </div> );
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
